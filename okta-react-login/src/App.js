c9dcfc5b2d34d70f158d37bb92631ba635fe094c
kerrysng <kerrysng@gmail.com>
2023-12-21 11:13:15 +1100

    Okta authentication

--- okta-react-login/src/App.js
+++ okta-react-login/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Security, SecureRoute, LoginCallback } from '@okta/okta-react';
+import { OktaAuth } from '@okta/okta-auth-js';
 
-function App() {
+const oktaAuth = new OktaAuth({
+  issuer: process.env.REACT_APP_OKTA_ORG_URL + '/oauth2/default',
+  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
+  redirectUri: window.location.origin + '/login/callback',
+  scopes: ['openid', 'profile', 'email'],
+  pkce: true
+});
+
+const Home = () => <h1>Home Page</h1>;
+const Protected = () => <h1>Protected Page</h1>;
+
+const Login = () => {
+  const login = async () => {
+    await oktaAuth.signInWithRedirect();
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login</h1>
+      <button onClick={login}>Login with Okta</button>
     </div>
   );
-}
+};
+
+const App = () => (
+  <Router>
+    <Security oktaAuth={oktaAuth}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/login/callback' element={<LoginCallback />} />
+        <Route path='/protected' element={<SecureRoute><Protected /></SecureRoute>} />
+      </Routes>
+    </Security>
+  </Router>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default App;
+
+
